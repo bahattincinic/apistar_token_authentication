5855ec0e7cc6104ac3bf63f051f0425f6033b5b3
Bahattin Çiniç <bahattincinic@gmail.com>
2018-02-05 23:48:48 +0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+setup(
+    name='apistar_token_auth',
+    version='1.0',
+    packages=['apistar_token_auth'],
+    url='https://github.com/bahattincinic/apistar_token_authentication ',
+    license='BSD',
+    description='Token Based Authentication for API Star',
+    author='Bahattin Cinic',
+    author_email='bahattincinic@gmail.com',
+    install_requires=['apistar'],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP',
+    ]
+)
